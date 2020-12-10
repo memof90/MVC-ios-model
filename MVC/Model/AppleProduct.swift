efe71b132c9a4e4d0b1a04ac774d50d2597a45f1
memo figueredo <memof80@gmail.com>
2020-12-10 17:10:21 -0500

    add model Layer Apple porduct

---
+++ MVC/Model/AppleProduct.swift
@@ -0,0 +1,26 @@
+//
+//  AppleProduct.swift
+//  MVC
+//
+//  Created by Memo Figueredo on 10/12/20.
+//
+
+import Foundation
+
+
+class AppleProduct {
+//    blueplan to product
+    var name : String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price:Double) {
+        self.name = name
+        self.color = color
+        self.price  = price
+    }
+}
+
+let appleProduct = AppleProduct(name: "Iphone 12", color: "Space Gray", price: 999.99)
+
+
