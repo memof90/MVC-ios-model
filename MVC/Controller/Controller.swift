9a9faef2c321e59ec2d1070ffd03b92d993d6264
memo figueredo <memof80@gmail.com>
2020-12-11 20:11:54 -0500

    add viewcontroller connect with model

--- MVC/Controller/Controller.swift
+++ MVC/Controller/Controller.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class Controller: UIViewController {
-
+    
+    // MARK: OUTLET
+    @IBOutlet weak var iphoneNameLabel: UILabel!
+    
+    @IBOutlet weak var iphoneColorLabel: UILabel!
+    
+    @IBOutlet weak var iphonePriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appleProduct = AppleProduct(name: "iphone X", color: "Space Gray", price: 999.99)
+        
+        iphoneNameLabel.text = appleProduct.name
+        iphoneColorLabel.text = "in \(appleProduct.color)"
+        iphonePriceLabel.text = "$\(appleProduct.price)"
+        
     }
 
 
